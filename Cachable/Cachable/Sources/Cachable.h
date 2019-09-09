86d3821d2d7236a4ae7f80c8a3957d88440ea11a
aramik <aramik@aramiks-MacBook-Pro-3.local>
2019-09-08 22:04:55 -0700

    added carthage dir structure with framework version

---
+++ Cachable/Cachable/Sources/Cachable.h
@@ -0,0 +1,19 @@
+//
+//  Cachable.h
+//  Cachable
+//
+//  Created by aramik on 9/8/19.
+//  Copyright © 2019 aramik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Cachable.
+FOUNDATION_EXPORT double CachableVersionNumber;
+
+//! Project version string for Cachable.
+FOUNDATION_EXPORT const unsigned char CachableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cachable/PublicHeader.h>
+
+
